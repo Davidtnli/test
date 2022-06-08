1a045241f0672b06a0d51cd6329992394f64e6d7
Davidtnli <105201222+Davidtnli@users.noreply.github.com>
2022-06-08 10:09:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config.update({
         openapi_version='2.0.0'
     ),
     'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
-    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+    'APISPEC_SWAGGER_UI_URL': '//'  # URI to access UI of API Doc
 })
 docs = FlaskApiSpec(app)
 
